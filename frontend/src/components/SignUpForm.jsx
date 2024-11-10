a3f364b840e9b3a1c58040c18cf8abaf1e68193b
lililimimimi <limimi916@gmail.com>
2024-11-10 15:52:45 +1100

    add the sign up form

---
+++ frontend/src/components/SignUpForm.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import React from "react";
+import Button from "@mui/material/Button";
+
+
+const SignUpForm = () => {
+  return (
+    <div>
+      <Button variant="contained">Contained</Button>
+    </div>
+  );
+  
+};
+
+export default SignUpForm;
